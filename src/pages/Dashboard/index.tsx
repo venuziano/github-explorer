42cd362b98cb6eac7bbfa3c44c3fd1eab115140c
venuziano <raf_rs@hotmail.com>
2020-09-16 21:30:30 -0300

    search on Repository ok and error handling ok

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,60 +1,80 @@
-import React from 'react';
+import React, { FormEvent, useState } from 'react';
 import { FiChevronRight } from 'react-icons/fi';
+
+import api from '../../services/api';
 
 import logoImg from '../../assets/logo.svg';
 
-import { Title, Form, Repositories } from './styles';
+import { Title, Form, Repositories, Error } from './styles';
+
+interface Repository {
+  full_name: string;
+  description: string;
+  owner: {
+    login: string;
+    avatar_url: string;
+  };
+}
 
 const Dashboard: React.FC = () => {
+  const [repositories, setRepositories] = useState<Repository[]>([]);
+  const [newRepo, SetNewRepo] = useState('');
+  const [inputError, setInputError] = useState('');
+
+  async function handleAddRepository(event: FormEvent<HTMLFormElement>): Promise<void> {
+    event.preventDefault();
+
+    if (!newRepo) {
+      setInputError('Digite o autor/nome do repositório.');
+      return;
+    }
+
+    try {
+      const response = await api.get<Repository>(`repos/${newRepo}`);
+
+      const repository = response.data;
+
+      setRepositories([...repositories, repository]);
+      SetNewRepo('');
+      setInputError('');
+    } catch (err) {
+      setInputError('Erro ao buscar repositório. Digite um nome válido.');
+    }
+  }
+
   return (
     <>
-    <img src={logoImg} alt="Explorer github" />
-    <Title>Explore repositórios no Github.</Title>
-    <Form>
-      <input placeholder="Informe o nome do repositório"/>
-      <button type="submit">Pesquisar</button>
-    </Form>
+      <img src={logoImg} alt="Github explorer" />
+      <Title>Explore repositórios no Github.</Title>
 
-    <Repositories>
-      <a href="teste">
-        <img
-          src="https://avatars3.githubusercontent.com/u/15386984?s=460&u=a927908b5d7306d6d5eb234da4094b4a9c7dbdb4&v=4"
-          alt="Rafael Rodrigues"
+      <Form hasError={!!inputError} onSubmit={handleAddRepository}>
+        <input
+          value={newRepo}
+          onChange={(e) => SetNewRepo(e.target.value)}
+          placeholder="Informe o nome do repositório."
         />
-        <div>
-          <strong>profys</strong>
-          <p>descrição do profys aqui</p>
-        </div>
+        <button type="submit">Pesquisar</button>
+      </Form>
 
-        <FiChevronRight size={20} />
-      </a>
+      { inputError && <Error>{inputError}</Error>}
 
-      <a href="teste">
-        <img
-          src="https://avatars3.githubusercontent.com/u/15386984?s=460&u=a927908b5d7306d6d5eb234da4094b4a9c7dbdb4&v=4"
-          alt="Rafael Rodrigues"
-        />
-        <div>
-          <strong>profys</strong>
-          <p>descrição do profys aqui</p>
-        </div>
+      <Repositories>
+        {repositories.map(repository => (
+          <a key={repository.full_name} href="teste">
+            <img
+              src={repository.owner.avatar_url}
+              alt={repository.owner.login}
+            />
+            <div>
+              <strong>{repository.full_name}</strong>
+              <p>{repository.description}</p>
+            </div>
 
-        <FiChevronRight size={20} />
-      </a>
+            <FiChevronRight size={20} />
+          </a>
+        ))}
 
-      <a href="teste">
-        <img
-          src="https://avatars3.githubusercontent.com/u/15386984?s=460&u=a927908b5d7306d6d5eb234da4094b4a9c7dbdb4&v=4"
-          alt="Rafael Rodrigues"
-        />
-        <div>
-          <strong>profys</strong>
-          <p>descrição do profys aqui</p>
-        </div>
-
-        <FiChevronRight size={20} />
-      </a>
-    </Repositories>
+      </Repositories>
 
     </>
   )
